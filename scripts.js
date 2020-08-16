43251c3797cea8a317e6e36b652d0e7e9d24ee15
Matheus Lima <matheus_sousa@protonmail.com>
2020-08-16 00:27:01 -0300

    div de maneira dinamica

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-function _numDias(){
-    var objData = new Date(),
-        numAno = objData.getFullYear(),
-        numMes = objData.getMonth()+1,
-        numDias = new Date(numAno, numMes, 0).getDate();
-  
-    return numDias;
+  var toAdd = document.createDocumentFragment();
+  for(var i=0; i < 11; i++){
+     var newDiv = document.createElement('div');
+     newDiv.id = 'r'+i;
+     newDiv.className = 'ansbox';
+     toAdd.appendChild(newDiv);
   }
   
+  document.appendChild(toAdd);
  
